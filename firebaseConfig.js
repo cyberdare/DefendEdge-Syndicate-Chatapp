e81de45fa426f7b45cab04d68c7e9bc195e1dd4d
cyberdare <104179495+cyberdare@users.noreply.github.com>
2024-03-28 12:11:01 +0530

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDkbhRIW1XSvADPC8_OeODIZB8PT7BnGy4",
+    authDomain: "defendedge-syndicate-chat-app.firebaseapp.com",
+    databaseURL: "https://defendedge-syndicate-chat-app-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "defendedge-syndicate-chat-app",
+    storageBucket: "defendedge-syndicate-chat-app.appspot.com",
+    messagingSenderId: "744220361596",
+    appId: "1:744220361596:web:a70ee7d31c69cf7d361af9",
+    measurementId: "G-4THBKS81M9"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
